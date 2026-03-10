34ab58bd7712ca6fbb1d594e51c4d6c3b138702e
Chong Rui Xin <rxchong1123@gmail.com>
2026-03-10 11:28:06 +0800

    added delete button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import { Badge, Card, Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { useContext } from "react";
+import TodoCard from "../components/TodoCard";
 import TodoContext from "../contexts/TodoContext";
 
 export function Home() {
@@ -16,17 +17,9 @@ export function Home() {
 
 function CardGroup({ todos }) {
   return todos.map((todo) => {
-    const completed = todo.completed;
-    const bg = completed ? "succsss" : "danger";
     return (
       <Col md={4} key={todo.id}>
-        <Card className="my-3">
-          <Card.Body>
-            <Card.Title>{todo.title}</Card.Title>
-            <Card.Text>{todo.description}</Card.Text>
-            <Badge bg={bg}>{!completed && "Not"} Completed</Badge>
-          </Card.Body>
-        </Card>
+        <TodoCard todo={todo} />
       </Col>
     );
   });
